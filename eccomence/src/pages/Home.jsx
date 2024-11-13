89118e349436777648d5269955f6d41e0a4d9c60
AyxanR <ahuseynli630@gmail.com>
2024-11-13 15:32:37 +0400

    Feedback

--- eccomence/src/pages/Home.jsx
+++ eccomence/src/pages/Home.jsx
@@ -8,57 +8,58 @@ import imgBracelet from "../images/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg";
 import imgBracelet2 from "../images/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg";
 import imgWD2TB from "../images/61IBBVJvSDL._AC_SY879_.jpg"
 
-function Home() {
-  const cardData = [
-    {
+const cardData = [
+  {
+    title: "Mens Casual",
+    image: imageCasual,
+    text: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight",
+    price: "$ 22,3",
+    BuyNow: "Buy Now",
+    AddToCard: "Add To Cart",
+  },
+  {
+    title: "Mens Cotton",
+    image: imageCotton,
+    text: "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as wor",
+    price: "$ 55.99",
+    BuyNow: "Buy Now",
+    AddToCard: "Add To Cart",
+  },
+  {
       title: "Mens Casual",
-      image: imageCasual,
-      text: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight",
-      price: "$ 22,3",
+      image: imageCasual2,
+      text: "The color could be slightly different between on the screen and in practice. / Please note",
+      price: "$ 15.99",
       BuyNow: "Buy Now",
       AddToCard: "Add To Cart",
-    },
-    {
-      title: "Mens Cotton",
-      image: imageCotton,
-      text: "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as wor",
-      price: "$ 55.99",
+  }, 
+  {
+      title: "John Hardy",
+      image: imgBracelet,
+      text: "From our Legends Collection, the Naga was inspired by the mythical water dragon that prote",
+      price: "$ 695",
       BuyNow: "Buy Now",
       AddToCard: "Add To Cart",
-    },
-    {
-        title: "Mens Casual",
-        image: imageCasual2,
-        text: "The color could be slightly different between on the screen and in practice. / Please note",
-        price: "$ 15.99",
-        BuyNow: "Buy Now",
-        AddToCard: "Add To Cart",
-    }, 
-    {
-        title: "John Hardy",
-        image: imgBracelet,
-        text: "From our Legends Collection, the Naga was inspired by the mythical water dragon that prote",
-        price: "$ 695",
-        BuyNow: "Buy Now",
-        AddToCard: "Add To Cart",
-    },
-    {
-        title: "Solid Gold",
-        image: imgBracelet2,
-        text: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by ",
-        price: "$ 168",
-        BuyNow: "Buy Now",
-        AddToCard: "Add To Cart",
-    },
-    {
-        title: "WD 2TB Eleme",
-        image: imgWD2TB,
-        text: "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity",
-        price: "$ 64",
-        BuyNow: "Buy Now",
-        AddToCard: "Add To Cart",
-    }
-  ];
+  },
+  {
+      title: "Solid Gold",
+      image: imgBracelet2,
+      text: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by ",
+      price: "$ 168",
+      BuyNow: "Buy Now",
+      AddToCard: "Add To Cart",
+  },
+  {
+      title: "WD 2TB Eleme",
+      image: imgWD2TB,
+      text: "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity",
+      price: "$ 64",
+      BuyNow: "Buy Now",
+      AddToCard: "Add To Cart",
+  }
+];
+
+function Home() {
 
   return (
     <>
